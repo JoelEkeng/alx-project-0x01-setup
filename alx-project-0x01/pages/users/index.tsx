5729ee906f7eeabeee65dd34c9dfa4e1f721e415
Joel Ekeng <joelekeng23@gmail.com>
2025-01-03 14:57:00 +0000

    Users

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,37 +2,48 @@ import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces/index";
 
-const Users: React.FC<UserProps> = ({ posts }) => {
-  return (
-    <div className="flex flex-col h-screen">
-    <Header />
-    <main className="p-4">
-      <div className="flex justify-between">
-      <h1 className=" text-2xl font-semibold">Post Content</h1>
-      <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
-      </div>
-      <div className="grid grid-cols-3 gap-2 ">
-        {
-          posts?.map(({ name, email, id, username, address, phone, website, company }: UserProps, key: number) => (
-            <UserCard name={name} email={email} id={id} username={username} address={address} phone={phone} website={website} company={company} key={key} />
-          ))
-        }
-      </div>
-    </main>
-  </div>
-  )
+interface UsersPageProps {
+  posts: UserProps[];
 }
 
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {posts?.map(({ name, email, id, username, address, phone, website, company }: UserProps, key: number) => (
+            <UserCard
+              name={name}
+              email={email}
+              id={id}
+              username={username}
+              address={address}
+              phone={phone}
+              website={website}
+              company={company}
+              key={key}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
-
 
 export default Users;
