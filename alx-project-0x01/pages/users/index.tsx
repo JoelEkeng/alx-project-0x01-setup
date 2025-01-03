d4145823f238bb018236cf6156e4e273a926b2fa
Joel Ekeng <joelekeng23@gmail.com>
2025-01-03 14:57:31 +0000

    Users

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -12,8 +12,8 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+          <h1 className="text-2xl font-semibold">User Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-3 gap-2">
           {posts?.map(({ name, email, id, username, address, phone, website, company }: UserProps, key: number) => (
