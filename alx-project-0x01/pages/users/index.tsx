b0f255e7d2ad8a76ac7e4cf304f8d1e21c852463
Joel Ekeng <joelekeng23@gmail.com>
2025-01-03 15:47:50 +0000

    User

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,19 +1,36 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal"; 
 import { UserProps } from "@/interfaces/index";
+import { useState } from "react";
 
 interface UsersPageProps {
   posts: UserProps[];
 }
 
 const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">User Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+          <button
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+            onClick={handleOpenModal}
+          >
+            Add User
+          </button>
         </div>
         <div className="grid grid-cols-4 w-full">
           {posts?.map((user: UserProps) => (
@@ -31,6 +48,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
           ))}
         </div>
       </main>
+      {isModalOpen && <UserModal onClose={handleCloseModal} />}
     </div>
   );
 };
